9c096627246449c5dbb5bcf418758446b947ea12
dinankou <nicolas.larderet@gmail.com>
2025-05-16 19:49:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,3 +28,7 @@ def ajouter_recette():
 @app.route("/", methods=["GET"])
 def home():
     return "API Émaux en ligne", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
