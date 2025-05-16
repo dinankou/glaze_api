fccb8c2f5b7336017a01edb3bb85d90cfd96b24f
dinankou <nicolas.larderet@gmail.com>
2025-05-16 23:09:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)
+
 DATA_FILE = "recettes.json"
 
 if not os.path.exists(DATA_FILE):
