caf0157cd64a7d5128065b2c357d3546dabacbb1
dinankou <nicolas.larderet@gmail.com>
2025-05-21 12:26:17 +0200

    Create models.py
    création BDD

---
+++ models.py
@@ -0,0 +1,40 @@
+from app import db
+from datetime import datetime
+
+class Matiere(db.Model):
+    __tablename__ = 'matiere'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String, unique=True, nullable=False)
+    type = db.Column(db.String, nullable=False)  # "base" ou "oxyde"
+    unite = db.Column(db.String, default="g")
+    quantite = db.Column(db.Float, default=0.0)
+
+    achats = db.relationship("Achat", backref="matiere", lazy=True)
+    compositions = db.relationship("Composition", backref="matiere", lazy=True)
+
+
+class Achat(db.Model):
+    __tablename__ = 'achat'
+    id = db.Column(db.Integer, primary_key=True)
+    matiere_id = db.Column(db.Integer, db.ForeignKey("matiere.id"), nullable=False)
+    quantite = db.Column(db.Float, nullable=False)
+    prix = db.Column(db.Float, nullable=False)
+    fournisseur = db.Column(db.String)
+    date = db.Column(db.Date, default=datetime.utcnow)
+
+
+class Recette(db.Model):
+    __tablename__ = 'recette'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String, unique=True, nullable=False)
+
+    compositions = db.relationship("Composition", backref="recette", lazy=True)
+
+
+class Composition(db.Model):
+    __tablename__ = 'composition'
+    id = db.Column(db.Integer, primary_key=True)
+    recette_id = db.Column(db.Integer, db.ForeignKey("recette.id"), nullable=False)
+    matiere_id = db.Column(db.Integer, db.ForeignKey("matiere.id"), nullable=False)
+    type = db.Column(db.String, nullable=False)  # "base" ou "oxyde"
+    pourcentage = db.Column(db.Float, nullable=False)
